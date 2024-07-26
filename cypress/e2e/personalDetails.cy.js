ed5cb9fbe05672a1c3a2908a65e163ec576c735a
MojisolaaaO <mojisolaotusheso@gmail.com>
2024-07-26 11:11:33 +0100

    update

--- cypress/e2e/personalDetails.cy.js
+++ cypress/e2e/personalDetails.cy.js
@@ -34,8 +34,8 @@ describe('Profile Details', () => {
         cy.get(selectors.middleNameField).clear().type("Olushola")
         cy.get(selectors.lastNameField).clear().type("Otusheso")
         cy.get(selectors.employeeID).clear().type("2428")
-        cy.get(selectors.otherID).clear().type("222")
-        cy.get(selectors.licence).clear().type("12345")
+        cy.get(selectors.otherID).clear().type("33930751")
+        cy.get(selectors.licence).clear().type("74730337")
         cy.get(selectors.licenceExpiryDate).click()
         cy.get(selectors.calendar).contains('23').click();
         cy.get('.--close').click();
@@ -44,6 +44,10 @@ describe('Profile Details', () => {
         cy.get(selectors.gender).click();
         cy.get(selectors.personalDetailsSaveBtn).click()
 
+        // //update profile pic 
+        // cy.get('.employee-image').click()
+        // cy.get('.employee-image-wrapper > .employee-image').attachFile("Me.jpg").wait(5000)
+        // cy.get('.oxd-button')
 
 
 
